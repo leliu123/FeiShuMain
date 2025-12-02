633e84eefd2b3fe2d7802552d5be67659dd96df7
lisizeng <l18840756059@163.com>
2025-12-02 23:09:26 +0800

    简单修改位置

--- app/src/main/java/com/feishu/feishumain/MainActivity.kt
+++ app/src/main/java/com/feishu/feishumain/MainActivity.kt
@@ -23,6 +23,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewScreenSizes
+import com.feishu.feature.FeiShuMainApp
 import com.feishu.feishumain.ui.theme.FeiShuMainTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,61 +35,5 @@ class MainActivity : ComponentActivity() {
                 FeiShuMainApp()
             }
         }
-    }
-}
-
-@PreviewScreenSizes
-@Composable
-fun FeiShuMainApp() {
-    var currentDestination by rememberSaveable { mutableStateOf(AppDestinations.HOME) }
-
-    NavigationSuiteScaffold(
-        navigationSuiteItems = {
-            AppDestinations.entries.forEach {
-                item(
-                    icon = {
-                        Icon(
-                            it.icon,
-                            contentDescription = it.label
-                        )
-                    },
-                    label = { Text(it.label) },
-                    selected = it == currentDestination,
-                    onClick = { currentDestination = it }
-                )
-            }
-        }
-    ) {
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-            Greeting(
-                name = "Android",
-                modifier = Modifier.padding(innerPadding)
-            )
-        }
-    }
-}
-
-enum class AppDestinations(
-    val label: String,
-    val icon: ImageVector,
-) {
-    HOME("Home", Icons.Default.Home),
-    FAVORITES("Favorites", Icons.Default.Favorite),
-    PROFILE("Profile", Icons.Default.AccountBox),
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FeiShuMainTheme {
-        Greeting("Android")
     }
 }
