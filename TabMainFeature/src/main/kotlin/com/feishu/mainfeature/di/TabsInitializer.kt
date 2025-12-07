7578269351119a47029c5564de5a7b83ba7bd02a
lzy <liuzhanyu159357@gmail.com>
2025-12-07 18:16:02 +0800

    制作了日历和消息tab

--- TabMainFeature/src/main/kotlin/com/feishu/mainfeature/di/TabsInitializer.kt
+++ TabMainFeature/src/main/kotlin/com/feishu/mainfeature/di/TabsInitializer.kt
@@ -14,6 +14,5 @@ import com.feishu.mainfeature.tabs.WorkplaceTab
 fun initTabs() {
     TabRegistry.register(MessageTabRegister()) // The original Message Tab
     TabRegistry.register(CalendarTab())      // The new Calendar Tab
-    TabRegistry.register(WorkplaceTab())     // The new Workplace Tab
     TabRegistry.register(ProfileTab())       // The new Profile Tab
 }
